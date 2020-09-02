97942f8c733359cc51d3cb0d2382033df5e875e1
Draylon <draylid@gmail.com>
2020-09-02 00:22:51 -0300

    Arrumei esse bug zoado

--- chebyshev.py
+++ chebyshev.py
@@ -1,19 +1,21 @@
 
+
+#   >>>>       https://github.com/Draylon/CHEBYSHEV_polinomios      <<<<<
+
+
+
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-
-def chev_cos(n,x):
-    if chebyshev_primeira_ordem(n,math.cos(x)) == math.cos(n*x):
-        return 1
-    return 0
+from sympy import *
+x= symbols('x')
 
 def chebyshev_primeira_ordem(n):
-    if n == 0:
-        return "1"
+    if n <= 0:
+        return '1'
     if n == 1:
-        return "x"
-    return "2*x*("+chebyshev_primeira_ordem(n-1)+")-("+chebyshev_primeira_ordem(n-2)+")"
+        return 'x'
+    return '((2*x)*('+chebyshev_primeira_ordem(n-1)+')-('+chebyshev_primeira_ordem(n-2)+'))'
 
 def chebyshev_segunda_ordem(n):
     if n == 0:
@@ -23,23 +25,41 @@ def chebyshev_segunda_ordem(n):
     return "2*x*("+chebyshev_segunda_ordem(n-1)+")-("+chebyshev_segunda_ordem(n-2)+")"
 
 
-xv = np.linspace(-1,1,35)
-for i in range(5):
-    y1s=chebyshev_primeira_ordem(i) #pegando a função de primeira ordem do chebyshev como string
-    y2s=chebyshev_segunda_ordem(i)  #esse aqui é o de segunda ordem
+n = 6
+
+#xv = np.linspace(-1,1,35) #lista de pontos de x
+for i in range(0,n):
     
+    y1s=chebyshev_primeira_ordem(i) #pegando a função de primeira ordem do chebyshev como string
+    #y2s=chebyshev_segunda_ordem(i)  #esse aqui é o de segunda ordem
     #print(y1s)
-    #print(y2s)
+    if(i % 2) != 0 and len(y1s) > 1:
+        y1s = str(simplify(y1s))
+        y1s = y1s[2:]
+        y1s = simplify(y1s)
+        print(simplify(f'{y1s}*{x}'))
+        print( [simplify(f'{y1s}').coeff(x,ii-1) for ii in range(i,0,-1)] ) # aqui, pega a string, simplifica e lista os coeficientes
+    else:
+        y1s = simplify(str(y1s))
+        print(y1s)
+        print( [simplify(f'{y1s}').coeff(x,ii) for ii in range(i,0,-1)] ) # aqui, pega a string, simplifica e lista os coeficientes
+
+    print()
+    #print(y1s)
     #printar no console
     
+    #essa parte desenha um gráfico
+    '''
     y1v=[eval(y1s) for x in xv] # interpretando as funções, e gerando uma lista de pontos pra plotar
-    y2v=[eval(y2s) for x in xv] # aqui, com a segunda ordem
     plt.plot(xv, y1v, '-b') # joga os pontos da primeira ordem
-    plt.plot(xv, y2v, '-r') # aqui os da segunda ordem
 plt.title('CHEBYSHEV')
 plt.xlabel('x', color='#1C2833')
 plt.ylabel('y', color='#1C2833')
 plt.grid()
 plt.show()
 
+'''
+
 # >.<'
+
+
